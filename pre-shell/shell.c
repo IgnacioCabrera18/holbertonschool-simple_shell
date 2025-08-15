dd19e92fe9173354d566d3158fd536e5b8af07fe
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-08-15 00:33:13 +0000

    super_simple_shell

--- pre-shell/shell.c
+++ pre-shell/shell.c
@@ -1,8 +1,8 @@
-#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 extern char **environ;
 
@@ -13,28 +13,55 @@ extern char **environ;
  *
  */
 
-int mini_shell()
+int main()
 {
-	pid_t pid;
-	char *line = NULL;
-	size_t len = 0;
+	char *line = NULL, *token, *argv[2];
+	size_t len = 0 ;
 	ssize_t read;
-	char *token, limite[] = " ";
+	pid_t pid;
+	int status;
 
 	while (1)
 	{
-		printf("minishell$ ");
+		printf("#minishell$ ");
 
 		read = getline(&line, &len, stdin);
+		if (read == -1)
+		{
+			printf("\n");
+			break;
+		}
 
-		token = strtok(line, limite);
+		if (line[read - 1] == '\n')
+			line[read - 1] = '\0';
+		if (line[0] == '\0')
+			continue;
+
+		token = strtok(line, " ");
+		if (token == NULL)
+			continue;
 
 		pid = fork();
-
-		if (pid == 0)
+		if (pid == -1)
 		{
-			execve(token[0], token, environ);
+			perror("fork");
 			exit(1);
 		}
+		else if (pid == 0)
+		{
+			argv[0] = token;
+			argv[1] = NULL;
+
+			execve(token, argv,environ);
+			perror("execve");
+			exit(1);
+		}
+		else
+		{
+			wait(&status);
+		}
 	}
+
+	free(line);
+	return (0);
 }

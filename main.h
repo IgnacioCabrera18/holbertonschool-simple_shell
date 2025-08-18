388e4154a869595a1a214a3c90700936301a699c
IgnacioCabrera18 <ignaciocabrera1832006@gmail.com>
2025-08-18 14:12:34 +0000

    our main.h

---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+
+
+
+extern char **environ;
+
+#endif
